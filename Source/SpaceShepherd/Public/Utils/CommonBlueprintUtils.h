cf0f0d919ad44e2acbc164c837046cb596dcc933
Thurinum <m.gagnon941@gmail.com>
2025-10-24 23:01:42 -0400

    refactor: Use development-only IMC for debug inputs

---
+++ Source/SpaceShepherd/Public/Utils/CommonBlueprintUtils.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CommonBlueprintUtils.generated.h"
+
+/**
+* Common routines exposed to Blueprint not tied to a specific game module
+*/
+UCLASS()
+class SPACESHEPHERD_API UCommonBlueprintUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure)
+	static bool IsDevelopmentOrEditorBuild()
+	{
+#if WITH_EDITOR || !UE_BUILD_SHIPPING
+		return true;
+#else
+		return false;
+#endif
+	}
+};
+
